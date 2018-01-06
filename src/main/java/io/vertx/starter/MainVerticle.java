134689d7354f4a3551214ccb05b6a77319d4b525
skyzhangty <skyzhangty@gmail.com>
2018-01-05 21:58:47 -0500

    first commit - to be amended

---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,12 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    startFuture.complete();
+  }
+}
